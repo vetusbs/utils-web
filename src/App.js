a2469731520edc9daff037e795c00a23308b1c49
albert.bres <albert.bres@skyscanner.net>
2021-01-13 09:38:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: "",
+    };
+    this.shuffle = this.shuffle.bind(this);
+  }
+
+  shuffle(e) {    
+    e.preventDefault();    
+    fetch('https://api-utils.herokuapp.com/random/string')
+    .then(res => res.text())
+    .then((data) => {
+      console.log(data)
+      this.setState({ text: data })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.shuffle}>RANDOM STRING</button>
+        <div>
+            {this.state.text}
+        </div>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
